1a9719fa950ed3a9289baf18884790f474296432
DianaTixi <tixidiana2@gmail.com>
2020-12-04 16:48:08 -0500

    commit

--- venta/src/controlador/ComprarLibroController.java
+++ venta/src/controlador/ComprarLibroController.java
@@ -61,8 +61,9 @@ public class ComprarLibroController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
